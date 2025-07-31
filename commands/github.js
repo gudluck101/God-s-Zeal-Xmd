a56c6a5af64ae73ce08f8b95a19d66445d3c04e9
AiOfLautech <babalolahephzibah2@gmail.com>
2025-07-31 14:28:55 -0500

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 async function githubCommand(sock, chatId, message) {
   try {
-    const res = await fetch('https://github.com/repos/AiOfLautech/God-s-Zeal-Xmd');
+    const res = await fetch('https://api.github.com/repos/AiOfLautech/God-s-Zeal-Xmd');
     if (!res.ok) throw new Error('Error fetching repository data');
     const json = await res.json();
 
